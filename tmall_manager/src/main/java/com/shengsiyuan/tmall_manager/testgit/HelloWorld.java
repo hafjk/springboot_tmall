5c961f460e6d716ec00bf2c47765cba14679956a
hafjk <54433513+hafjk@users.noreply.github.com>
2019-11-21 10:00:37 +0800

    Update HelloWorld.java

--- tmall_manager/src/main/java/com/shengsiyuan/tmall_manager/testgit/HelloWorld.java
+++ tmall_manager/src/main/java/com/shengsiyuan/tmall_manager/testgit/HelloWorld.java
@@ -3,5 +3,6 @@ package com.shengsiyuan.tmall_manager.testgit;
 public class HelloWorld {
     public void output(){
         System.out.print("hello");
+        System.out.print("zengdema ");
     }
 }
